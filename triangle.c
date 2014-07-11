6a32d3942af2e113123d707886555ac66fa87210
Delyan Angelov <delian66@gmail.com>
2014-07-11 20:58:12 +0300

    Split up the program to separate functions.

--- triangle.c
+++ triangle.c
@@ -1,30 +1,42 @@
 #include "GL/gl.h"
 #include "SDL/SDL.h"
 
-int main()
-{
-  SDL_Event event;
+void initgraphics(){
+   SDL_SetVideoMode(640,480,0,SDL_OPENGL);
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   glFrustum(-1.33,1.33,-1,1,1.5,100);
+   glMatrixMode(GL_MODELVIEW);
+   glEnable(GL_DEPTH_TEST);
+   glClear(GL_DEPTH_BUFFER_BIT|GL_COLOR_BUFFER_BIT);
+   glLoadIdentity();
+}
 
-  SDL_SetVideoMode(640,480,0,SDL_OPENGL|SDL_FULLSCREEN);
-  SDL_ShowCursor(SDL_DISABLE);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glFrustum(-1.33,1.33,-1,1,1.5,100);
-  glMatrixMode(GL_MODELVIEW);
-  glEnable(GL_DEPTH_TEST);
-  glClear(GL_DEPTH_BUFFER_BIT|GL_COLOR_BUFFER_BIT);
+////////////////////////////////////////////
 
-  glLoadIdentity();
-  glBegin(GL_TRIANGLES);
-  glVertex3i(1,1,-10);
-  glVertex3i(1,-1,-10);
-  glVertex3i(-1,1,-10);
-  glEnd();
-  SDL_GL_SwapBuffers();
-  
-  do
-  {
-    SDL_PollEvent(&event);
-  } while (event.type!=SDL_KEYDOWN);
-  SDL_Quit();
+void on_update(){
+   glBegin(GL_TRIANGLES);
+   glVertex3i(1,1,-10);
+   glVertex3i(1,-1,-10);
+   glVertex3i(-1,1,-10);
+   glEnd();
+   SDL_GL_SwapBuffers();
+}
+
+////////////////////////////////////////////
+
+void loop(){
+   SDL_Event event;   
+   do{
+      on_update();
+      SDL_PollEvent(&event);
+   }while (event.type!=SDL_KEYDOWN);      
+   SDL_Quit();
+}
+
+////////////////////////////////////////////
+
+int main(){
+   initgraphics();   
+   loop();
 }
